4f810df78cf7f55cb72079f2daffdad74ecd4574
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 01:27:46 -0300

    feat: Add create product use case

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,26 @@
+import path from 'node:path';
+
 import { Router } from 'express';
+import multer from 'multer';
 
 import { listProducts } from '../app/useCases/products/listProducts';
+import { createProduct } from '../app/useCases/products/createProduct';
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, callback) {
+            callback(null, path.resolve(__dirname, '..', '..', 'uploads'));
+        },
+        filename(req, file, callback) {
+            callback(null, `${Date.now()}-${file.originalname}`);
+        }
+    })
+});
 
 const productRoutes = Router();
 
 productRoutes.get('/', listProducts);
 productRoutes.get('/:id/products');
-productRoutes.post('/');
+productRoutes.post('/', upload.single('image'), createProduct);
 
 export { productRoutes };
