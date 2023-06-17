90d1445ea3618c4329e185f7810d85c69d6a08b8
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 00:40:29 -0300

    feat: Add create category use case

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import { listCategories } from '../app/useCases/categories/listCategories';
+import { createCategory } from '../app/useCases/categories/createCategory';
 
 const categoryRoutes = Router();
 
 categoryRoutes.get('/', listCategories);
-categoryRoutes.post('/');
+categoryRoutes.post('/', createCategory);
 
 
 export { categoryRoutes };
