e5d8d9bb6aa7a7faba78c248426c57ab372d15c5
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 15:55:45 -0300

    feat: Add chenge order status use case validation

--- src/app/useCases/orders/changeOrderStatus.ts
+++ src/app/useCases/orders/changeOrderStatus.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import { Order } from '../../models/Order';
+import { AppError } from '../../error/AppError';
 
 export async function changeOrderStatus(req: Request, res: Response) {
     const { id } = req.params;
     const { status } = req.body;
 
-    if(!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
-        return res.status(400).json({
-            error: 'Status should be one of these: WAITING, IN_PRODUCTION, DONE'
-        });
+    const isValidId = mongoose.Types.ObjectId.isValid(id);
+
+    if(!isValidId) {
+        throw new AppError('Invalid order id!');
     }
 
-    await Order.findByIdAndUpdate(id, { status });
+    const isOrderExists = await Order.findById(id);
+
+    if(!isOrderExists) {
+        throw new AppError('Order not found!', 404);
+    }
+
+    if(!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)) {
+        throw new AppError('Status should be one of these: WAITING, IN_PRODUCTION, DONE');
+    }
 
     return res.sendStatus(204);
 }
