22648f7f4b892891f390adc14dbe8c9f3c544c43
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 02:19:24 -0300

    feat: Add cancel order use case

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,13 +3,14 @@ import { Router } from 'express';
 import { listOrders } from '../app/useCases/orders/listOrders';
 import { createOrder } from '../app/useCases/orders/createOrder';
 import { changeOrderStatus } from '../app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from '../app/useCases/orders/deleteOrder';
 
 const orderRoutes = Router();
 
 orderRoutes.get('/', listOrders);
 orderRoutes.post('/', createOrder);
 orderRoutes.patch('/:id', changeOrderStatus);
-orderRoutes.delete('/:id');
+orderRoutes.delete('/:id', cancelOrder);
 
 
 export { orderRoutes };
