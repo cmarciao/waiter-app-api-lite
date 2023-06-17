4b834aae05429d2dda5c311e5f2610caff6d2bed
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 02:02:25 -0300

    feat: Add create order use case

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import { listOrders } from '../app/useCases/orders/listOrders';
+import { createOrder } from '../app/useCases/orders/createOrder';
 
 const orderRoutes = Router();
 
 orderRoutes.get('/', listOrders);
-orderRoutes.post('/');
+orderRoutes.post('/', createOrder);
 orderRoutes.patch('/:id');
 orderRoutes.delete('/:id');
 
