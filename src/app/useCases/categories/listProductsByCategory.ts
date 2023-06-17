c77e57b3585b560e31e25fc16afebcc52a904035
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 14:58:33 -0300

    feat: Add list products by category use case validation

--- src/app/useCases/categories/listProductsByCategory.ts
+++ src/app/useCases/categories/listProductsByCategory.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import { Product } from '../../models/Product';
+import { Category } from '../../models/Category';
+
+import { AppError } from '../../error/AppError';
 
 export async function listProductsByCategory(req: Request, res: Response) {
     const { id } = req.params;
+
+    const isValidId = mongoose.Types.ObjectId.isValid(id);
+
+    if(!isValidId) {
+        throw new AppError('Category id is not valid!', 400);
+    }
+
+    const isCategoryExists = await Category.findById(id);
+
+    if(!isCategoryExists) {
+        throw new AppError('Category not found', 404);
+    }
 
     const products = await Product.find().where('category').equals(id);
 
