a3d1702d7138c2ee270024384f3375facec30c71
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 01:24:06 -0300

    feat: Add list products use case

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
+import { listProducts } from '../app/useCases/products/listProducts';
+
 const productRoutes = Router();
 
-productRoutes.get('/');
+productRoutes.get('/', listProducts);
 productRoutes.get('/:id/products');
 productRoutes.post('/');
 
