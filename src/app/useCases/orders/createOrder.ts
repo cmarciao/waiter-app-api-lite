dbc27e25214c0d27093b02bfe709e7dd7d3ba962
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 16:02:43 -0300

    feat: Add create orde use case validation

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from 'express';
 
 import { Order } from '../../models/Order';
+import { Product } from '../../models/Product';
+
+import { AppError } from '../../error/AppError';
 
 export async function createOrder(req: Request, res: Response) {
     const { table, products } = req.body;
+
+    if(!table) {
+        throw new AppError('Table is required!');
+    }
+
+    for(const item of products) {
+        const isProductExists = await Product.findById(item.product);
+
+        if(!isProductExists) {
+            throw new AppError('Some product not found!');
+        }
+    }
 
     const order = await Order.create({ table, products });
 
