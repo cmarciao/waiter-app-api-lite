c9c98a437757dd715fd5a324df24f1046bae5384
Cássio Marcião <marciaocassio@gmail.com>
2023-06-17 14:51:43 -0300

    feat: Add create category use case validation

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from 'express';
 
 import { Category } from '../../models/Category';
+import { AppError } from '../../error/AppError';
 
 export async function createCategory(req: Request, res: Response) {
     const { icon, name } = req.body;
+
+    if(!icon) {
+        throw new AppError('Icon is required!');
+    }
+
+    if(!name) {
+        throw new AppError('Name is required!');
+    }
+
+    const isCategoryExists = await Category.find().where('name').equals(name);
+
+    if(isCategoryExists.length != 0) {
+        throw new AppError('Category already exists!');
+    }
 
     const category = await Category.create({ name, icon });
 
